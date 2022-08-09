c2220a3bb3459ec5a87fc46973a73186dc19d059
root <4908@holbertonschool.com>
2022-08-09 11:39:02 -0700

    files

--- monty.h
+++ monty.h
@@ -35,6 +35,12 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+typedef struct arg
+{
+	char *argument;
+} arg;
+arg Arg;
+
 /*Function Prototypes*/
 
 
