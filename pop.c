618b82bbecf8846528275c03353aa51a8326c9fb
chostler24 <connor.hostler@gmail.com>
2022-08-09 14:09:05 -0700

    Adding pop function

---
+++ pop.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+  * pop - deletes node from stack/queue
+  * @stack: node
+  * @line_number: line number
+  * Return: none
+  */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%i: stack already empty", line_number);
+
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+
+	*stack = (*stack)->next;
+
+	free(temp);
+}
