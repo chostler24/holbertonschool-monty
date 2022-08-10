49a28c9de0d1bbb5d46c48fed35fd77e92ebb4e0
chostler24 <connor.hostler@gmail.com>
2022-08-10 09:19:52 -0700

    Updating header file

--- monty.h
+++ monty.h
@@ -50,5 +50,8 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif
