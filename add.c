ea64186a6822e0f6693fb54293b4c8585d6ba676
root <4908@holbertonschool.com>
2022-08-12 06:52:30 -0700

    bugfix

--- add.c
+++ add.c
@@ -16,10 +16,13 @@ void add(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%i: can't add, stack too short\n", line_number);
-
 		exit(EXIT_FAILURE);
 	}
-
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%i: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	temp = (*stack)->next;
 	sumof = (*stack)->n + temp->n;
 	temp->n = sumof;
